27ec950cf017420f1b43fbbc2aedf7b7d0d8bcea
Xu <whitenight.xiaxu@gmail.com>
2023-10-14 21:56:23 +0800

    initiate project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+  </div>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue"
+</script>
+
+<style scoped></style>
