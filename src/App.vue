7219593ae9b7ff56ec0e45f2bee8885471ecc963
夏 <whitenight2xia@gmail.com>
2023-10-27 21:43:46 +0800

    feat config basic

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,10 @@
   <div class="wrapper">
     <HelloWorld msg="You did it!" />
   </div>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue"
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <style scoped></style>
